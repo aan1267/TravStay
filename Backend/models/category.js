c17aa1c9cc6eccf426a4455de3efa1887501631b
aach1267 <aanchalsharmax1@gmail.com>
2024-11-19 18:10:26 +0530

    add changes

--- Backend/models/category.js
+++ Backend/models/category.js
@@ -2,18 +2,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const categorySchema =new Schema({
-  name: { 
-    type: String, 
-    required: true 
-  },
-  cities: [
-    {
-        name:{type:String,required:true},
-        description:{type:String,required:true}
-    }
-  ]
-});
-
-const Category= mongoose.model("Category",categorySchema)
+  name:String,
+  cities: [{
+      name:String,
+      description:String,
+   }]
+  })
+  const Category= mongoose.model("Category",categorySchema)
 
 module.exports=Category
