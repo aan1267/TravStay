50b1fdec336c64cc611eafe681956f671582ffbc
aach1267 <aanchalsharmax1@gmail.com>
2024-11-19 23:51:25 +0530

    change

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import './index.css'
 import HomeLandingPage from './components/home/HomeLandingPage.jsx'
-// import Footer from './components/home/Footer.jsx';
-// import Popular from "./components/home/Popular.jsx";
+
 
 const router=createBrowserRouter([
   {
