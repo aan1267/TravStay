01c00c39794875aaf542c8eaf3f0cb48a9f0b563
aach1267 <aanchalsharmax1@gmail.com>
2024-11-24 01:24:44 +0530

    change

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import './index.css'
 import HomeLandingPage from './components/home/HomeLandingPage.jsx'
+import Login from "./components/Login.jsx";
+import { SearchProvider } from "./components/home/SearchContext.jsx";
+import Signup from "./components/Signup.jsx";
 
 
 const router=createBrowserRouter([
@@ -11,7 +14,16 @@ const router=createBrowserRouter([
   element: 
       <HomeLandingPage />
   },
-   
+  {
+    path:"/login",
+    element:
+     <SearchProvider><Login/></SearchProvider>
+  },
+  {
+    path:"/signup",
+    element:
+     <SearchProvider><Signup/></SearchProvider>
+  }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
