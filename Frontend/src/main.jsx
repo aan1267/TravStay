29f766e2841767dc9fc2675a82701d737e254607
aach1267 <aanchalsharmax1@gmail.com>
2024-12-14 23:38:08 +0530

    add changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,36 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import './index.css'
-import HomeLandingPage from './components/home/HomeLandingPage.jsx'
-import Login from "./components/Login.jsx";
-import { SearchProvider } from "./components/home/SearchContext.jsx";
-import Signup from "./components/Signup.jsx";
+import Login from "./components/Login.jsx"
+import Signup from "./components/Signup.jsx"
+import Listing from "./components/Listing.jsx"
+import AllImages from "./components/AllImages.jsx"
+import LayoutComponent from "./components/home/LayoutComponent.jsx"
+import Hero from "./components/home/Hero.jsx"
+import { AuthProvider } from "./components/AuthContext.jsx";
+
+
 
 
 const router=createBrowserRouter([
   {
-    path: "/",
-  element: 
-      <HomeLandingPage />
-  },
-  {
-    path:"/login",
-    element:
-     <SearchProvider><Login/></SearchProvider>
-  },
-  {
-    path:"/signup",
-    element:
-     <SearchProvider><Signup/></SearchProvider>
-  }
+    element:<AuthProvider/>,
+    children:[
+      {
+      path: "/",
+      element:<LayoutComponent/>,
+      children:[
+        {
+          index: true,
+          element: 
+            <Hero />
+        },
+        {
+          path:"/listing/:id",
+          element:<Listing/>
+        },
+        {
+          path:"/login",
+          element:
+           <Login/>
+        },
+        {
+          path:"/signup",
+          element:
+           <Signup/>
+        }
+    ]},
+    {
+      path:"/listing/:id/all-photos",
+      element:<AllImages/>
+    },
+  ]}
 ])
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+
+
+
 
 
