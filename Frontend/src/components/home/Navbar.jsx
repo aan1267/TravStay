f214784fdd5233fb84bb3e7f3b35274072647c8e
aach1267 <aanchalsharmax1@gmail.com>
2024-12-22 22:46:55 +0530

    add changes

--- Frontend/src/components/home/Navbar.jsx
+++ Frontend/src/components/home/Navbar.jsx
@@ -1,45 +1,69 @@
 import React, { useRef, useState, useEffect,useContext } from "react"
 import "../../style/Navbar.css"
 import { SearchContext } from "./SearchContext"
+import {Link, useNavigate} from "react-router-dom"
+import { useAuth } from "../AuthContext"
+import Avatar from '@mui/material/Avatar'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
 
+    const {username,logout,isAuthenticate}=useAuth()
+    // console.log(username)
+  
   const dropdownRef = useRef(null)
   const { setSearchInput } = useContext(SearchContext)
-
+  const navigate=useNavigate()
+  
+  // useEffect(()=>{
+  // if( window.location.pathname === "signup"){
+  //     setSearchInput("")
+  //     setIsOpen(false)
+  //   }
+  // },[window.location.pathname,setSearchInput])
+  
   //!dropdownRef.current.contains(e.target)) detect click happen outside the dropdown
   const handleClickOutside = (e) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)  ) {
       setIsOpen(false)
     }
   };
   useEffect(() => {
     document.addEventListener("click", handleClickOutside)
-  }, []);
+  }, [])
 
   const toggleDropDown = (e) => {
-    e.stopPropagation()
-    setIsOpen((prev) => !prev);
-  };
+     e.stopPropagation()
+    setIsOpen((prev) => !prev)
+  }
+
+  const handleLogout=()=>{
+    console.log("logout")
+       logout()
+       setIsOpen(false)
+  }
 
   return (
     <div className="navbar">
+      <Link to="/">
       <div className="header">
         <img
           className="logo"
           src="/Travsaty-icon.png"
-          alt="Travstay"
-        />
+          alt="Travstay"/>
         <h1>Travstay</h1>
       </div>
+      </Link>
       <div>
-      <div className="search-bar">
-      <input type="text" placeholder="Search here..." onChange={(e)=> setSearchInput(e.target.value)}/>
-      <span>
-      <i class="fa-solid fa-magnifying-glass"></i>
-      </span>
-    </div>
+      {window.location.pathname === "/" &&(
+         <div className="search-bar">
+         <input type="text" placeholder="Search here..." onChange={(e)=> setSearchInput(e.target.value)}/>
+         <span>
+         <i class="fa-solid fa-magnifying-glass"></i>
+         </span>
+       </div>
+      )}
       </div>
       <div className="header-list">
         <p>Explore your home</p>
@@ -49,25 +73,34 @@ export default function Navbar() {
               <i class="fa-solid fa-bars"></i>
             </span>
             <span>
-              <i class="fa-solid fa-circle-user" id="user-icon"></i>
+            { isAuthenticate ? 
+               <Avatar style={{background:"blue"}}>{username}</Avatar>
+             : <AccountCircleIcon id="user-icon" style={{ fontSize: 40}}/>}
             </span>
           </button>
           {isOpen && (
             <div className="dropdown" ref={dropdownRef}>
               <ul>
                 <li>
-                  <button style={{fontWeight:"600"}}>Signup</button>
+                  <button style={{fontWeight:"600"}}  onClick={()=>navigate("/signup")}>Signup</button>
                 </li>
                 <li>
-                  <button>Login</button>
+                   <button onClick={()=>navigate("/login")}>Login</button>
                 </li>
                 <hr/>
                 <li>
-                  <button>Logout</button>
+                  <button onClick={handleLogout}>Logout</button>
                 </li>
                 <li>
                   <button>Help Center</button>
                 </li>
+                {
+                  isAuthenticate && (
+                  <li>
+                   <button onClick={()=>navigate("/account")}>Profile</button>
+                  </li>
+                  )
+                }
               </ul>
             </div>
           )}
