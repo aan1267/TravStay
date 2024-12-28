09278e3a7321e627c931810a330b2aefaf59fab1
aach1267 <aanchalsharmax1@gmail.com>
2024-12-28 11:31:27 +0530

    add changes

---
+++ Backend/middleware/authenticate.js
@@ -0,0 +1,26 @@
+const jwt=require("jsonwebtoken")
+require("dotenv").config()
+const userModel=require("../models/user")
+
+const authenticate=async(req,res,next)=>{
+      try{
+         const token=req.headers.authorization
+         console.log("token:",token)
+         const verifytoken=jwt.verify(token,process.env.JWT_SECRET)
+
+         const user=await userModel.findOne({_id:verifytoken._id})
+         if(!user){
+            throw new Error("user not found")
+         }
+         req.token=token
+         req.user=user
+         req.userId=user._id
+
+         next()
+       }catch(e){
+        res.status(401).json({status:401,message:"Unauthorized no token provide"})
+       }
+   }
+
+
+module.exports={authenticate}
