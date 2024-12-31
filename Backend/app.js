bdf17f7651501d3560a39a9e1e07c3abbe910803
aach1267 <aanchalsharmax1@gmail.com>
2024-12-31 23:33:30 +0530

    add changes:

--- Backend/app.js
+++ Backend/app.js
@@ -1,100 +1,80 @@
 const express=require("express")
 const app=express()
-const mongoose=require("mongoose")
-require('dotenv').config()
-const data=require("./data.js")
-const Listing= require("./models/listing.js")
-const Category=require("./models/category.js")
+const connectDB=require("./config/dbconfig.js")
 const cors=require("cors")
+const cookieParser=require("cookie-parser")
 
-app.use(cors())
-const Mongo_Url=process.env.MONGO_URL;
+// const data=require("./data.js")
+
+//import routes
+const listingRoutes=require("./routes/listingRoutes.js")
+const categoryRoutes = require("./routes/categoriesRoutes.js")
+const userRoute = require("./routes/userRoutes.js")
+const imageRouter=require("./routes/imageRoute.js")
 
 
-main()
-.then(()=>console.log("connected sucessfully"))
-.catch((e)=>console.log(e))
-async function main(){
-    await mongoose.connect(Mongo_Url)
-}
+//middleware
+app.use(cors({
+    origin:'*',  
+  credentials: true,  
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
+// app.use("/uploads",express.static(__dirname+"/uploads"))
 
-app.get("/",(req,res)=>{
-    res.send("hello")
-})
-// const categories = [
-//     {
-//       name:"popular",
-//       cities: [
-//         { name: "Canmore", description: "Chalet rentals" },
-//         { name: "Benalmádena", description: "Beach house rentals"},
-//         { name: " Marbella", description: "Beachfront rentals" },
-//         { name: " Prescott", description: " Cottage rentals"},
-//         { name: "Scoddal", description: "Pet-friendly rentals"},
-//         { name: "Mijas", description: "Flat rentals"},
-//       ]
-//     },
-//     {
-//       name:"arts and culture",
-//       cities: [
-//         { name: "Phoenix", description: "Rentals with pools"},
-//         { name: "Washington", description: "Beach house rentals" },
-//         { name: "Hot Springs", description: "Lakehouse rentals" },
-//         { name: "Prague", description: "Holiday rentals" },
-//         { name: "San Francisco", description: "Beach house rentals" },
-//         { name: "Dublin", description: "Cottage rentals" },
-//       ]
-//     },
-//     {
-//         name:"outdoors",
-//         cities: [
-//             { name: "Canmore", description: "Chalet rentals" },
-//             { name: "Benalmádena", description: "Beach house rentals",},
-//             { name: " Marbella", description: "Beachfront rentals" },
-//             { name: " Prescott", description: " Cottage rentals"},
-//             { name: "Scoddal", description: "Pet-friendly rentals"},
-//             { name: "Mijas", description: "Flat rentals"},
-//         ]
-//     },
-//     {
-//         name:"mountains",
-//         cities: [
-//           { name: "Dauphin Island", description: "Chalet rentals"},
-//           { name: "Moonta Bay", description: "Beach house rentals"},
-//           { name: "Marbella", description: "house rentals"},
-//           { name: "Prescott", description: "Cottage rentals"},
-//           { name: "Bruny Island", description: "flat rentals"},
-//           { name: "Scoddal", description: "Pet friendly"},
-//         ]
-//       },
-//       {
-//         name:"beach",
-//         cities: [
-//         { name: "Phoenix", description: "Rentals with pools"},
-//         { name: "Washington", description: "Beach house rentals" },
-//         { name: "Hot Springs", description: "Lakehouse rentals" },
-//         { name: "Prague", description: "Holiday rentals" },
-//         { name: "San Francisco", description: "Beach house rentals" },
-//         { name: "Dublin", description: "Cottage rentals" },
-//         ]
-//     }
-//   ];
+//use routes
+app.use("/listing",listingRoutes)
+app.use("/categories",categoryRoutes)
+app.use("/jwt",userRoute)
+app.use("/api",imageRouter)
 
-app.get("/insert",async(req,res)=>{
-    await Category.insertMany(categories)
-    console.log("data category")
-})
 
-app.get("/listing",async(req,res)=>{
-    let listings=await Listing.find({})
-    res.status(202).json({listings})
-    // res.send("done!")
-})
 
-app.get("/categories",async(req,res)=>{
-    let categories=await Category.find()
-    res.status(202).json({categories})
-})
+connectDB()
+
+
+
+
+// app.get("/",(req,res)=>{
+//     res.send("hello")
+// })
+
+// app.get("/insert",async(req,res)=>{
+//     await Category.insertMany(categories)
+//     console.log("data category")
+// })
+
 
 app.listen(8080,()=>{
     console.log("server is listen to 8080 port");
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
