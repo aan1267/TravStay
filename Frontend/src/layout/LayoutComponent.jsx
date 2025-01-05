6b1385a5d3dff57a85f8374f934c5226b9bc6c11
aach1267 <aanchalsharmax1@gmail.com>
2025-01-05 23:43:23 +0530

    ...

---
+++ Frontend/src/layout/LayoutComponent.jsx
@@ -0,0 +1,31 @@
+import React,{useState,useEffect} from "react"
+import Navbar from "../layout/Navbar"
+import Footer from "../layout/Footer.jsx"
+import { Outlet,useLocation} from "react-router-dom"
+import  {SearchProvider} from "../components/SearchContext.jsx"
+
+
+
+
+export default function LayoutComponent(){
+  const [hidefooter ,setHideFooter]=useState(false)
+  const location=useLocation()
+  useEffect(()=>{
+    if(location.pathname.startsWith("/account/")|| location.pathname.startsWith("/account")){
+      setHideFooter(true)
+    }else{
+      setHideFooter(false)
+    }
+  },[location.pathname])
+  return (
+    <div className="app-container">
+       <SearchProvider>
+         <Navbar/> 
+         <main className="main-content">
+         <Outlet/>
+         </main>
+         {!hidefooter && <Footer/>}
+       </SearchProvider>
+    </div>
+  )
+}
---
+++ Frontend/src/layout/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { useRef, useState, useEffect, useContext } from "react";
+import "../styles/Navbar.css";
+import { SearchContext } from "../components/SearchContext";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../components/AuthContext";
+import Avatar from "@mui/material/Avatar";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { username, logout, isAuthenticate ,setisAuthenticate} = useAuth();
+  // console.log(username)
+
+  const dropdownRef = useRef(null);
+  const { setSearchInput } = useContext(SearchContext);
+  const navigate = useNavigate();
+
+  // useEffect(()=>{
+  // if( window.location.pathname === "signup"){
+  //     setSearchInput("")
+  //     setIsOpen(false)
+  //   }
+  // },[window.location.pathname,setSearchInput])
+
+  // !dropdownRef.current.contains(e.target))  detect click happen outside the dropdown
+  const handleClickOutside = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)  ) {
+      setIsOpen(false)
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside)
+  }, [])
+
+  const toggleDropDown = (e) => {
+    e.stopPropagation();
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleLogout = () => {
+    logout()
+    setIsOpen(false)
+    setisAuthenticate(false)
+  };
+
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <div className="header">
+          <img className="logo" src="/Travsaty-icon.png" alt="Travstay" />
+          <h1>Travstay</h1>
+        </div>
+      </Link>
+      <div>
+        {window.location.pathname === "/" && (
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search here..."
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <span>
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </span>
+          </div>
+        )}
+      </div>
+      <div className="header-list">
+        <p>Explore your home</p>
+        <div className="profile-box">
+          <button
+            className="profile-btn"
+            id="dropdownMenuButton"
+            aria-haspopup="true"
+            aria-expanded={isOpen ? "true" : "false"}
+            onClick={toggleDropDown}
+          >
+            <span className="profile">
+              <i class="fa-solid fa-bars"></i>
+            </span>
+            <span>
+              {isAuthenticate ? (
+                <Avatar style={{ background: "blue" }}>{username}</Avatar>
+              ) : (
+                <AccountCircleIcon id="user-icon" style={{ fontSize: 40 }} />
+              )}
+            </span>
+          </button>
+           <div className={`dropdown-menu ${isOpen ? 'show' : ''}`}  ref={dropdownRef} aria-labelledby="dropdownMenuButton">
+              <Link to="/login"class="dropdown-item" onClick={()=>setIsOpen(false)}>
+                Login
+              </Link>
+              <Link to="/signup" class="dropdown-item" onClick={()=>setIsOpen(false)}>
+                Signup
+              </Link>
+              <Link to="/" class="dropdown-item" onClick={handleLogout}>
+                Logout
+              </Link>
+              {isAuthenticate && (
+                  <Link to="/account" onClick={()=>setIsOpen(false)} class="dropdown-item">Profile  
+                  </Link>
+                  )
+                }
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
