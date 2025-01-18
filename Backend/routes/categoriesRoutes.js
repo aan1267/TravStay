a890727f723958619fdfcf8365c296981cd8f319
aach1267 <aanchalsharmax1@gmail.com>
2025-01-19 02:01:12 +0530

    add changes

--- Backend/routes/categoriesRoutes.js
+++
@@ -1,7 +0,0 @@
-const express=require("express")
-const categoryRoute=express.Router()
-const categoryController=require("../controllers/categoryController.js")
-
-categoryRoute.get("/",categoryController.getAllCategories)
-
-module.exports=categoryRoute
