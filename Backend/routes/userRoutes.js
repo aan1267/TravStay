0b511abf820633d529a67d9f69d96310c914c1ab
aach1267 <aanchalsharmax1@gmail.com>
2025-01-19 21:33:30 +0530

    Some more changes added

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,7 +3,6 @@ const userController=require("../controllers/userController.js")
 const userRouter=express.Router()
 const {authenticate}=require("../middleware/authenticate.js")
 
-
 userRouter.post("/signup",userController.jwtSignup)
 userRouter.post("/login",userController.jwtLogin)
 userRouter.get("/validuser",authenticate,userController.validuser)
