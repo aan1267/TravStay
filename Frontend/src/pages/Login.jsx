253510119adb0bbe79a582ff295dfa41328ef052
aach1267 <aanchalsharmax1@gmail.com>
2025-04-02 17:55:57 +0530

    small changes

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -8,7 +8,7 @@ import axios from "axios"
 import {useNavigate} from "react-router-dom"
 import {ToastContainer,toast} from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
-import { useAuth } from "../context/AuthContext"
+import { useAuth } from "../context/AuthProvider"
 
 function Login() {
   // const [email, setEmail] = useState("");
