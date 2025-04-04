2c801b274501dbeaebf61bd48df8bf87cfef0b7a
aach1267 <aanchalsharmax1@gmail.com>
2025-04-04 17:18:18 +0530

    Fix:some bugs

--- Frontend/src/layout/Navbar.jsx
+++ Frontend/src/layout/Navbar.jsx
@@ -1,36 +1,39 @@
 import React, { useRef, useState, useEffect, useContext } from "react";
 import "../styles/Navbar.css";
-import { SearchContext } from "../context/SearchContext";
+import { SearchContext } from "../context/SearchProvider";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/AuthProvider";
 import Avatar from "@mui/material/Avatar";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { FaBars, FaSearch } from 'react-icons/fa';
-
+import { FaBars, FaSearch } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
-
-  const { username, logout, isAuthenticate,setisAuthenticate} = useAuth();
+  const { username, logout, isAuthenticate, setisAuthenticate } = useAuth();
   // console.log(username)
 
   const dropdownRef = useRef(null);
-  const { setSearchInput } = useContext(SearchContext);
+  const { searchInput, setSearchInput } = useContext(SearchContext);
   const navigate = useNavigate();
-  
- 
+
+
 
   // !dropdownRef.current.contains(e.target))  detect click happen outside the dropdown
   const handleClickOutside = (e) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(e.target)  ) {
-      setIsOpen(false)
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setIsOpen(false);
     }
   };
- 
+
+  const handleSearchClear=()=>{
+     setSearchInput("");
+  }
+
   useEffect(() => {
-    document.addEventListener("click", handleClickOutside)
-  }, [])
+    document.addEventListener("click", handleClickOutside);
+  }, []);
 
   const toggleDropDown = (e) => {
     e.stopPropagation();
@@ -38,74 +41,96 @@ export default function Navbar() {
   };
 
   const handleLogout = () => {
-    logout()
-    setIsOpen(false)
+    logout();
+    setIsOpen(false);
   };
 
   return (
     <div className="navbar">
-      <div className="container">
-      <Link to="/" className="text-decoration-none">
-        <div className="header d-flex align-items-center">
-          <img className="logo" src="/Travsaty-icon.png" alt="Travstay" />
-          <h1>Travstay</h1>
-        </div>
-      </Link>
-      <div>
-        {window.location.pathname === "/" && (
-          <div className="search-bar">
-            <input
-              type="text"
-              placeholder="Search here..."
-              onChange={(e) => setSearchInput(e.target.value)}
-            />
-              <span>
-              <FaSearch />
-              </span>
+      <div className="container-fluid">
+        <Link to="/" className="text-decoration-none">
+          <div className="header d-flex align-items-center">
+            <img className="logo" src="/Travsaty-icon.png" alt="Travstay" />
+            <h1>Travstay</h1>
           </div>
-        )}
-      </div>
-      <div className="header-list">
-        <p>Explore your home</p>
-        <div className="profile-box">
-          <button
-            className="profile-btn"
-            id="dropdownMenuButton"
-            aria-haspopup="true"
-            aria-expanded={isOpen ? "true" : "false"}
-            onClick={toggleDropDown}
-          >
-            <span className="profile">
-            <FaBars/>
-            </span>
-            <span>
-              {isAuthenticate ? (
-                <Avatar className="bg-primary">{username}</Avatar>
-              ) : (
-                <AccountCircleIcon className="fs-1" />
-              )}
-            </span>
-          </button>
-           <div className={`dropdown-menu ${isOpen ? 'show' : ''}`}  ref={dropdownRef} aria-labelledby="dropdownMenuButton">
-              <Link to="/login"class="dropdown-item" onClick={()=>setIsOpen(false)}>
+        </Link>
+        <div>
+          {window.location.pathname === "/" && (
+            <div className="search-bar">
+              <input
+                type="text"
+                value={searchInput}
+                placeholder="Search here..."
+                onChange={(e) => setSearchInput(e.target.value)}
+              />
+              <span>
+                {searchInput ? (
+                  <button onClick={handleSearchClear} className="border-0 bg-white">
+                    <FaXmark />
+                  </button>
+                ) : (
+                  <FaSearch />
+                )}
+              </span>
+            </div>
+          )}
+        </div>
+        <div className="header-list">
+          <p>Explore your home</p>
+          <div className="profile-box">
+            <button
+              className="profile-btn"
+              id="dropdownMenuButton"
+              aria-haspopup="true"
+              aria-expanded={isOpen ? "true" : "false"}
+              onClick={toggleDropDown}
+            >
+              <span className="profile">
+                <FaBars />
+              </span>
+              <span>
+                {isAuthenticate ? (
+                  <Avatar className="bg-primary">{username}</Avatar>
+                ) : (
+                  <AccountCircleIcon className="fs-1" />
+                )}
+              </span>
+            </button>
+            <div
+              className={`dropdown-menu ${isOpen ? "show" : ""}`}
+              ref={dropdownRef}
+              aria-labelledby="dropdownMenuButton"
+            >
+              <Link
+                to="/login"
+                class="dropdown-item"
+                onClick={() => setIsOpen(false)}
+              >
                 Login
               </Link>
-              <Link to="/signup" class="dropdown-item" onClick={()=>setIsOpen(false)}>
+              <Link
+                to="/signup"
+                class="dropdown-item"
+                onClick={() => setIsOpen(false)}
+              >
                 Signup
               </Link>
               <Link to="/" class="dropdown-item" onClick={handleLogout}>
                 Logout
               </Link>
               {isAuthenticate && (
-                  <Link to="/account" onClick={()=>setIsOpen(false)} class="dropdown-item">Profile  
-                  </Link>
-                  )
-                }
+                <Link
+                  to="/account"
+                  onClick={() => setIsOpen(false)}
+                  class="dropdown-item"
+                >
+                  Profile
+                </Link>
+              )}
             </div>
+          </div>
         </div>
       </div>
-  </div>
-  </div>
+    </div>
   );
 }
-
