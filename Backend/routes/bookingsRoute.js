c6039c73bd4640ae7b11583049fef0bb557b3b9d
aach1267 <aanchalsharmax1@gmail.com>
2025-11-03 23:49:35 +0530

    rename this

--- Backend/routes/bookingsRoute.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const bookingsRoute=express.Router()
-const BookingsController=require("../controllers/BookingsController.js")
-const {authenticate}=require("../middleware/authenticate.js")
-
-bookingsRoute.post("/",authenticate,BookingsController.bookings)
-bookingsRoute.get("/",authenticate,BookingsController.getbooking)
-
-module.exports=bookingsRoute
