b011425f9e504f252c67cb33226f74e55b365b46
aach1267 <aanchalsharmax1@gmail.com>
2025-11-22 23:46:35 +0530

    make code more clean by adding errorHandler middleware use here

--- Backend/controllers/imageController.js
+++ Backend/controllers/imageController.js
@@ -1,46 +1,30 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 
-// console.log({ __dirname})
-const uploadByLink=async(req,res)=>{
-    try{
-      const {link}=req.body
-      const newName= "photo" + Date.now()
-      const result = await cloudinary.uploader.upload(link,{
-        public_id:  'TravStay-images/' + newName,
-        folder: "TravStay-images"
-      })
-      // console.log(result.url)
-      res.json({imageUrl:result.url})
-  }catch(e){
-    res.status(500).json({ message:"error during upload by link"});
+const uploadByLink = async (req, res, next) => {
+  try {
+    const { link } = req.body;
+    const newName = "photo" + Date.now();
+    const result = await cloudinary.uploader.upload(link, {
+      public_id: "TravStay-images/" + newName,
+      folder: "TravStay-images",
+    });
+    // console.log(result.url)
+    res.json({ imageUrl: result.url });
+  } catch (e) {
+    next(e);
   }
-}
+};
 
-
-
-const uploadImages= async(req,res)=>{
-    // console.log(req.files)
-    let uploadfiles=[]
-     try{
-       for(let i=0;i<req.files.length;i++){
-           const file=req.files[i]
-        if(file.path){
-           uploadfiles.push(file.path)
-          }
-        }
-         res.status(200).json({ uploadfiles })
-         console.log(uploadfiles)
-      }catch(e){
-            return res.status(500).json({ message: "Error uploading image" })
-       }
+const uploadImages = async (req,res,next) => {
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).json({ msg: "No files uploaded" });
   }
+  try {
+    const uploadfiles = req.files.map((f) => f.path); //Cloudinary URLs
+    res.status(200).json({ uploadfiles });
+  } catch (e) {
+    next(e);
+  }
+};
 
-module.exports={uploadByLink,uploadImages}
-
-
-
-
-
-
-
-       
+module.exports = { uploadByLink, uploadImages };
