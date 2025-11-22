a24f4500d224820ac59a8b38910274d1a5b0d238
aach1267 <aanchalsharmax1@gmail.com>
2025-11-22 23:52:47 +0530

    add fileSize , files max upload

--- Backend/middleware/upload-middleware.js
+++ Backend/middleware/upload-middleware.js
@@ -1,6 +1,12 @@
-const multer=require("multer")
-const {storage}=require("../config/cloudinaryconfig.js")
+const multer = require("multer");
+const { storage } = require("../config/cloudinaryconfig.js");
 
-const upload = multer({storage:storage })
+const upload = multer({
+  storage: storage,
+  limits: { 
+    fileSize:  2 * 1024 * 1024, // 2MB limit
+    files: 6 // max 6 page allowed 
+},
+});
 
-module.exports = upload
+module.exports = upload;
